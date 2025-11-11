b92a1743190ce76b1cfceb000ad96d656cdfbcb7
ddrsql <ddrsql>
2025-11-11 22:18:56 +0800

    VoloAbp-Integrated-OTel

---
+++ VoloAbp/src/VoloAbp.OTel/OTel/OTelActivityAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VoloAbp.OTel
+{
+    /// <summary>
+    /// Activity (Span) 
+    /// Span 表示跟踪中的单个操作。Span 可以嵌套，形成跟踪树。
+    /// 每个跟踪包含一个根 Span，通常描述整个操作，并且（可选）还包含一个或多个用于描述其子操作的子 Span。
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Interface)]
+    public class OTelActivityAttribute : Attribute
+    {
+        public OTelActivityAttribute()
+        {
+        }
+
+        public OTelActivityAttribute(bool isDisabled)
+        {
+            IsDisabled = isDisabled;
+        }
+
+        /// <summary>
+        /// 是否禁用
+        /// </summary>
+        public bool IsDisabled { get; set; }
+    }
+}
---
+++ VoloAbp/src/VoloAbp.OTel/OTel/VoloAbpOTelModule.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Volo.Abp.DynamicProxy;
+using Volo.Abp.Modularity;
+using Volo.Abp.Uow;
+
+namespace VoloAbp.OTel
+{
+    public class VoloAbpOTelModule : AbpModule
+    {
+        public override void PreConfigureServices(ServiceConfigurationContext context)
+        {
+            if (context.Services.GetConfiguration().GetValue("OTelOptions:Enabled", false))
+                context.Services.OnRegistered(OTelActivityInterceptorRegistrar.RegisterIfNeeded);
+        }
+
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            var configuration = context.Services.GetConfiguration();
+            Configure<OTelOptions>(configuration.GetSection(OTelOptions.Key));
+        }
+    }
+}
