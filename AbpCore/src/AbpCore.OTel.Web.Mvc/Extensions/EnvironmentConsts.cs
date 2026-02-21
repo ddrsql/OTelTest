88f956ea1e65d1e455a8bc05b21bf4f7555cdc1d
ddrsql <ddrsql>
2026-02-21 14:55:44 +0800

    AbpCoreOTel-Integrated-OTel

---
+++ AbpCore/src/AbpCore.OTel.Web.Mvc/Extensions/EnvironmentConsts.cs
@@ -0,0 +1,7 @@
+﻿namespace Microsoft.AspNetCore.Extensions;
+
+public static class EnvironmentConsts
+{
+    public const string ASPNETCORE_APPLICATION = "ASPNETCORE_APPLICATION";
+    public const string ASPNETCORE_ENVIRONMENT = "ASPNETCORE_ENVIRONMENT";
+}
